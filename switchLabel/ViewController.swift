27e054790d62720ee64b7dc36f4fd0447a38500f
Magnus Kruschwitz <magnuskruschwitz@magnus-macbook-pro.local>
2019-01-31 07:22:12 +0100

    Initial Commit

---
+++ switchLabel/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  switchLabel
+//
+//  Created by Magnus Kruschwitz on 31.01.19.
+//  Copyright © 2019 Magnus Kruschwitz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
